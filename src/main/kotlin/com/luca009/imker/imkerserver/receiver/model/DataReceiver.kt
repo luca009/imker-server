1c7f793c73592bafc3a061139b218ccbe4f4ed5e
luca009 <realluca009@gmail.com>
2024-01-21 11:06:34 +0100

    Update DataReceiver

--- src/main/kotlin/com/luca009/imker/imkerserver/receiver/model/DataReceiver.kt
+++ src/main/kotlin/com/luca009/imker/imkerserver/receiver/model/DataReceiver.kt
@@ -1,7 +1,5 @@
 package com.luca009.imker.imkerserver.receiver.model
 
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import java.time.ZonedDateTime
 
 interface DataReceiver {
@@ -10,9 +8,11 @@ interface DataReceiver {
      * If no file is found for the specified time is found, the next-earliest file will be chosen instead. If there is no earlier file available, no file is downloaded.
      * Returns a DownloadResult indicating whether the operation was successful, as well as the file path, if applicable.
      *
-     * Note: This function is blocking. It will halt the current thread's execution until all data is downloaded.
+     * Note: This function is blocking. It will halt the current thread's execution until all data is obtained.
      */
     fun downloadData(dateTime: ZonedDateTime, downloadedFileName: String?): DownloadResult
+
+    fun updateNecessary(dateTime: ZonedDateTime): Boolean
 }
 
 interface AromeReceiver : DataReceiver
