8f1ef323f05604ca85368311e33d7e42d4dee516
luca009 <realluca009@gmail.com>
2024-05-07 11:20:43 +0200

    Update queue improvements (#14)
    * Fix dependent job removal when job fails and use Duration for updateTimeInterval
    
    * Rename removeJobDependencies to removeJobDependents

--- src/main/kotlin/com/luca009/imker/server/configuration/properties/UpdateProperties.kt
+++ src/main/kotlin/com/luca009/imker/server/configuration/properties/UpdateProperties.kt
@@ -2,12 +2,13 @@ package com.luca009.imker.server.configuration.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import java.time.Duration
 
 @Configuration
 @ConfigurationProperties(prefix = "update")
 class UpdateProperties {
     var lazyUpdate: Boolean = false
-    var updateCheckInterval: Int = 10
+    var updateCheckInterval: Duration = Duration.ofMinutes(10)
 
     var lazyCaching: Boolean = false
 }
