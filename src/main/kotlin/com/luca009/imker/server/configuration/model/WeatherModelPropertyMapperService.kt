eac7f4298f058dcc9affc6bd0ffb286627b0344a
luca009 <realluca009@gmail.com>
2024-06-16 13:29:06 +0200

    Documentation for other configuration mappers

--- src/main/kotlin/com/luca009/imker/server/configuration/model/WeatherModelPropertyMapperService.kt
+++ src/main/kotlin/com/luca009/imker/server/configuration/model/WeatherModelPropertyMapperService.kt
@@ -2,6 +2,12 @@ package com.luca009.imker.server.configuration.model
 
 import java.util.SortedMap
 
+/**
+ * Mapper interface for creating [WeatherModel]s from the local configuration
+ */
 interface WeatherModelPropertyMapperService {
+    /**
+     * Get the weather models assembled from the configuration
+     */
     fun getWeatherModels(): SortedMap<Int, WeatherModel>
 }
