d6c129408000c409cecf9a882df7405c7887963e
luca009 <realluca009@gmail.com>
2024-06-16 13:23:33 +0200

    Documentation for WeatherModel.kt

--- src/main/kotlin/com/luca009/imker/server/configuration/model/WeatherModel.kt
+++ src/main/kotlin/com/luca009/imker/server/configuration/model/WeatherModel.kt
@@ -6,15 +6,57 @@ import com.luca009.imker.server.management.files.model.LocalFileManagementConfig
 import com.luca009.imker.server.parser.model.WeatherDataParser
 import com.luca009.imker.server.receiver.model.DataReceiver
 
+/**
+ * Represents a weather model
+ */
 data class WeatherModel(
+    /**
+     * The name used for the weather model. This is used when making requests to the endpoints and to represent the model in responses.
+     */
     val name: String,
+
+    /**
+     * The friendly name of the weather model. Used in responses as optional additional information.
+     */
     val friendlyName: String,
+
+    /**
+     * The copyright information for the weather model. Used in responses as optional additional information.
+     */
     val copyright: String,
+
+    /**
+     * The [DataReceiver] used to obtain the weather data files
+     */
     val receiver: DataReceiver,
+
+    /**
+     * The [WeatherDataParser] used to parse the weather data files
+     */
     val parser: WeatherDataParser,
+
+    /**
+     * The [WeatherVariableTypeMapper] used to map the variable names/identifiers to [com.luca009.imker.server.parser.model.WeatherVariableType]s
+     */
     val mapper: WeatherVariableTypeMapper,
+
+    /**
+     * The [DataFileNameManager] used to manage the local files
+     */
     val fileNameManager: DataFileNameManager,
+
+    /**
+     * THe [WeatherVariableUnitMapper] used to map the unit strings to [com.luca009.imker.server.parser.model.WeatherVariableUnit]s
+     */
     val unitMapper: WeatherVariableUnitMapper,
+
+    /**
+     * The [WeatherRasterCompositeCacheConfiguration] that is to be used by any [com.luca009.imker.server.caching.model.WeatherRasterCompositeCache] storing this weather model
+     */
     val cacheConfiguration: WeatherRasterCompositeCacheConfiguration,
+
+    /**
+     * The [LocalFileManagementConfiguration] that is to be used by any [com.luca009.imker.server.management.files.model.LocalFileManagerService] managing the local files for this weather model
+     */
     val fileManagementConfiguration: LocalFileManagementConfiguration
 )
