e284acbb9a3a6857f8b6bf5ea7e3aa70679891b8
luca009 <realluca009@gmail.com>
2024-06-17 20:48:51 +0200

    Documentation for components regarding file management

--- src/main/kotlin/com/luca009/imker/server/management/files/model/BestFileSearchService.kt
+++ src/main/kotlin/com/luca009/imker/server/management/files/model/BestFileSearchService.kt
@@ -1,12 +1,15 @@
 package com.luca009.imker.server.management.files.model
 
 import org.springframework.context.annotation.Scope
- import java.time.Duration
 import java.time.ZonedDateTime
 
 @Scope("singleton")
 interface BestFileSearchService {
-    fun <T> getBestFile(files: Map<T, String?>, referenceDateTime: ZonedDateTime, fileNameManager: DataFileNameManager): Triple<T, ZonedDateTime, Duration>?
+    /**
+     * Get the closest (but earlier) file compared to the [referenceDateTime].
+     * [files] represents a [Map] with any key and a string as value, representing the file name, which gets processed with the [fileNameManager].
+     */
+    fun <T> getBestFile(files: Map<T, String?>, referenceDateTime: ZonedDateTime, fileNameManager: DataFileNameManager): T?
 }
 
 class BestFileNotFoundException(message: String? = null) : IllegalArgumentException(message)
--- src/main/kotlin/com/luca009/imker/server/management/files/model/LocalFileManagerService.kt
+++ src/main/kotlin/com/luca009/imker/server/management/files/model/LocalFileManagerService.kt
@@ -6,9 +6,23 @@ import java.nio.file.Path
 import java.time.Duration
 import java.time.ZonedDateTime
 
+/**
+ * Interface for managing storage locations for local files
+ */
 interface LocalFileManagerService {
+    /**
+     * Get the path to the specified [storageLocation] (and optionally, with a [subFolder])
+     */
     fun getWeatherDataLocation(storageLocation: String, subFolder: String? = null): Path
+
+    /**
+     * Get files by the specified [weatherModel] that are to be cleaned up as based on the configured storage policy
+     */
     fun getFilesForCleanup(weatherModel: WeatherModel, referenceDateTime: ZonedDateTime = ZonedDateTime.now()): Set<Pair<File, ZonedDateTime>>?
+
+    /**
+     * Cleanup files by the specified [weatherModel] based on the configured storage policy
+     */
     fun cleanupWeatherDataLocation(weatherModel: WeatherModel, referenceDateTime: ZonedDateTime = ZonedDateTime.now()): Boolean
 }
 
