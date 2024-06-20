9c2ddcca6a00852ff2ff58745297e3d4ce8caf44
luca009 <realluca009@gmail.com>
2024-06-20 20:09:48 +0200

    Forgot to commit BestFileSearchService.kt oops

--- src/main/kotlin/com/luca009/imker/server/management/files/model/BestFileSearchService.kt
+++ src/main/kotlin/com/luca009/imker/server/management/files/model/BestFileSearchService.kt
@@ -1,11 +1,15 @@
 package com.luca009.imker.server.management.files.model
 
 import org.springframework.context.annotation.Scope
- import java.time.Duration
+import java.time.Duration
 import java.time.ZonedDateTime
 
 @Scope("singleton")
 interface BestFileSearchService {
+    /**
+     * Get the closest (but earlier) file compared to the [referenceDateTime].
+     * [files] represents a [Map] with any key and a string as value, representing the file name, which gets processed with the [fileNameManager].
+     */
     fun <T> getBestFile(files: Map<T, String?>, referenceDateTime: ZonedDateTime, fileNameManager: DataFileNameManager): Triple<T, ZonedDateTime, Duration>?
 }
 
