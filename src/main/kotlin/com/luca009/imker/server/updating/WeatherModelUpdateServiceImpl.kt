64234ad0b9a7dd1bf1f08fde801d55b42486ef05
luca009 <realluca009@gmail.com>
2024-06-21 21:11:00 +0200

    Fix updateCheckInterval

--- src/main/kotlin/com/luca009/imker/server/updating/WeatherModelUpdateServiceImpl.kt
+++ src/main/kotlin/com/luca009/imker/server/updating/WeatherModelUpdateServiceImpl.kt
@@ -1,19 +1,17 @@
 package com.luca009.imker.server.updating
 
-import com.luca009.imker.server.configuration.properties.UpdateProperties
 import com.luca009.imker.server.management.models.WeatherModelUpdateJobEnabled
 import com.luca009.imker.server.management.models.model.WeatherModelManagerService
 import com.luca009.imker.server.updating.model.WeatherModelUpdateService
 import kotlinx.coroutines.runBlocking
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.util.concurrent.TimeUnit
 
 @Component
 class WeatherModelUpdateServiceImpl(
     private val weatherModelManagerService: WeatherModelManagerService
 ) : WeatherModelUpdateService() {
-    @Scheduled(fixedRateString = "\${update.updateCheckInterval}", initialDelayString = "\${update.updateCheckInterval}", timeUnit = TimeUnit.MINUTES)
+    @Scheduled(fixedRateString = "\${update.updateCheckInterval}", initialDelayString = "\${update.updateCheckInterval}")
     fun scheduledUpdateWeatherModels() = runBlocking {
         // TODO: Implement lazy updating
         weatherModelManagerService.beginUpdateWeatherModels()
